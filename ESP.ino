1ad49444526c899b1251c3f9795422f99506e2ea
Naffik <76112481+Naffik@users.noreply.github.com>
2021-06-08 20:30:19 +0200

    Add files via upload

--- ESP.ino
+++ ESP.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 #include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
 #include <String>
 
 WiFiServer server(80);
@@ -7,11 +8,6 @@ Servo servo;
 
 const char* ssid = "XD 2.4"; // Your SSID
 const char* password = "516489723"; //Your password
-int inPin = 12; // D6
-int outPin = 14; // D5
-int val = 0;   
-char pass = 'o';
-char clo = 'c';
 
 IPAddress local_IP(192, 168, 0, 46); // Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1); // Set your IP address
@@ -19,12 +15,71 @@ IPAddress subnet(255, 255, 255, 0); // Set your network mask
 IPAddress primaryDNS(8, 8, 8, 8);   //optional
 IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
+int inPin = 12; // D6
+int outPin = 14; // D5
+int val = 0;   
+char pass = 'o';
+char clo = 'c';
+int authorization_required = 1;
+
+WebSocketsServer webSocket = WebSocketsServer(81); //websocket init with port 81
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+//webscket event method
+    String cmd = "";
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.println("Websocket is disconnected");
+            //case when Websocket is disconnected
+            break;
+        case WStype_CONNECTED:{
+            //wcase when websocket is connected
+            Serial.println("Websocket is connected");
+            Serial.println(webSocket.remoteIP(num).toString());
+            webSocket.sendTXT(num, "connected");}
+            break;
+        case WStype_TEXT:
+            cmd = "";
+            Serial.println(cmd);
+            for(int i = 0; i < length; i++) {
+                cmd = cmd + (char) payload[i]; 
+            } //merging payload to single string
+            
+            if(cmd == "open"){
+               servo.write(0);
+            }else if(cmd == "close"){
+              servo.write(90);
+              digitalWrite(outPin, HIGH);
+              delay(1000);
+              digitalWrite(outPin, LOW);
+              val = 0;
+              delay(1000);
+            }
+             webSocket.sendTXT(num, cmd+":success");
+             //send response to mobile, if command is "poweron" then response will be "poweron:success"
+             //this response can be used to track down the success of command in mobile app.
+            break;
+        case WStype_FRAGMENT_TEXT_START:
+            break;
+        case WStype_FRAGMENT_BIN_START:
+            break;
+        case WStype_BIN:
+            hexdump(payload, length);
+            break;
+        default:
+            break;
+    }
+}
+
+
 void setup() {
   pinMode(inPin, INPUT);
   pinMode(outPin, OUTPUT);
   servo.attach(4);
-  servo.write(90); 
+  servo.write(70);
   Serial.begin(115200);
+  Serial.println("Connecting to wifi");
+   
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("STA Failed to configure");
   }
@@ -39,41 +94,16 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+   webSocket.begin(); //websocket Begin
+   webSocket.onEvent(webSocketEvent); //set Event for websocket
+   Serial.println("Websocket is started");
   server.begin();
   digitalWrite(outPin, LOW);
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  val = digitalRead(inPin);
-  //Serial.println(val);
-  delay(2000);
   
   if(val){
-    Serial.println("test");
-    if(client){
-      while(client.connected()){
-        while(client.available() > 0){
-          char c = client.read();
-          if(c == pass){
-            servo.write(0);
-          }else if(c == clo){
-            servo.write(90);
-            digitalWrite(outPin, HIGH);
-            delay(1000);
-            digitalWrite(outPin, LOW);
-            client.stop();
-            val = 0;
-            delay(1000);
-          }
-          Serial.write(c);
-        }
-        delay(10);
-      }
-      client.stop();
-      Serial.println("Client disconnected");
-    }
-  }else{
-    servo.write(90);  
+    webSocket.loop();
   }
 }
